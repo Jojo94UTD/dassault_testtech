f6b94cf00f94977ea3a6b3434e5f1a79a48a2137
Jojo94UTD <jordan_tchuem@yahoo.fr>
2025-02-26 20:37:44 +0100

    Initialisation du projet Spring Boot pour le test technique Dassault

---
+++ src/main/java/com/testDassault/droneDassault/DroneDassaultApplication.java
@@ -0,0 +1,13 @@
+package com.testDassault.droneDassault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDassaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDassaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testDassault/droneDassault/DroneDassaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testDassault.droneDassault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDassaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
